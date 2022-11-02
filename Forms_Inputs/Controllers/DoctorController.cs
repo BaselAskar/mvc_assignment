14bf9e4a78978da709ab11d9806d94a643b2353a
Basel Askar <baselaskar91@gmail.com>
2022-11-02 16:58:40 +0100

    Add project files.

---
+++ Forms_Inputs/Controllers/DoctorController.cs
@@ -0,0 +1,35 @@
+﻿using Forms_Inputs.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forms_Inputs.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult TestFever()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult TestFever(double temp)
+        {
+            if (temp == 0)
+            {
+                ViewBag.Message = "Place insert the degree";
+            }
+            else
+            {
+                ViewBag.Message = FeverResult.Test(temp);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Forms_Inputs/Helpers/FeverResult.cs
@@ -0,0 +1,25 @@
+﻿namespace Forms_Inputs.Helpers
+{
+    public class FeverResult
+    {
+        public static string Test(double temp)
+        {
+            
+            if (temp < 30)
+                    return "Yes had to be die you are like a freaze....";
+
+            if (temp < 37)
+                    return "you are cold you have to get warm ....";
+
+            if(temp == 37)
+                    return "you are so good. your tempreture is normal .. perfect.";
+
+            if (temp > 37 && temp < 40)
+                return "you have a fever .. you have to go home and take a medicine";
+
+            else
+                return "you are so hot ... it's dangerous";
+            
+        }
+    }
+}
